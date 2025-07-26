58bb434c27947c6479818284d10523fddd0e3031
BenjaminS <rayn.stenger@gmail.com>
2025-07-26 18:49:38 +0200

    Ajout page prestations avec modal
    Création de la page prestations avec modal, utilisation d'objets pour simplifier la répétition des sections

--- Mspr-Canopees/src/components/Modal.jsx
+++ Mspr-Canopees/src/components/Modal.jsx
@@ -2,17 +2,14 @@ export default function Modal({ open, onClose, photos }) {
   if (!open) return null;
 
   return (
-    <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
-      <div className="bg-[var(--light-white)] p-6 rounded-lg max-w-lg w-full relative">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-xl font-bold text-rouge"
-        >
-          ✕
-        </button>
-        <div className="grid grid-cols-1 gap-4">
+    <div className="fixed inset-0 bg-black/80 flex items-center justify-center z-50"
+    onClick={onClose}
+    >
+      <div className="bg-none p-6 rounded-lg w-full relative align-center"
+      >
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 overflow-y-auto">
           {photos.map((photo, i) => (
-            <img key={i} src={photo} alt="réalisation" className="rounded-lg" />
+            <img key={i} src={photo} alt="réalisation" className="w-150 h-150 object-contain rounded-lg self-center mx-auto" />
           ))}
         </div>
       </div>
