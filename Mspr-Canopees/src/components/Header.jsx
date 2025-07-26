0349557859b575fef212d0d39ed36e0d1d90e4b9
BenjaminS <rayn.stenger@gmail.com>
2025-07-26 17:15:25 +0200

    Classement des images dans dossier about et création de la page présentation
    Page présentation, reprise des sections acceuil, reprise du carousel, changement des photos responsive à faire

--- Mspr-Canopees/src/components/Header.jsx
+++ Mspr-Canopees/src/components/Header.jsx
@@ -1,14 +1,28 @@
+import { useLocation } from "react-router-dom";
 import Navbar from "../Navbar";
 import Logo from "../assets/Accueil/logo-canopees.png"
 
 export default function Header() {
+  const location = useLocation();
+  const titles = {
+    "/": "Donnez vie à vos espaces verts",
+    "/about": "Qui sommes-nous",
+    "/prestations": "Nos prestations",
+    "/tarifs": "Nos tarifs",
+    "/contact": "Contactez-nous",
+    "/cgu": "Conditions d'utilisation",
+    "/cgv": "Conditions générales de vente",
+    "/mentions": "Mentions légales",
+  };
+  const title = titles[location.pathname] || "Canopées";
+
   return (
     <header className="w-full h-125 relative text-[var(--light-white)]">
 
       <hero-section className="w-full h-125 z-10 bg-[url(./assets/Accueil/hero-bg.jpg)] bg-no-repeat bg-cover bg-fixed bg-position-[center_bottom_6rem] absolute inset-0">
         <div className="absolute inset-0 bg-black/40">
           <Navbar />
-          <h1 className="absolute top-30 right-45 flex justify-center p-4">Donnez vie à vos espaces verts</h1>
+          <h1 className="absolute top-30 right-45 flex justify-center p-4">{title}</h1>
           <div className="absolute z-1 bottom-5 right-48 mb-4">
             <img src={Logo} alt="Logo de Canopees" className="h-22 w-auto" />
           </div>
@@ -18,11 +32,8 @@ export default function Header() {
               w-[90%] h-48
               bg-gradient-to-r from-[var(--green)] to-[var(--orange)]
               z-0
+              [clip-path:polygon(0%_0%,90%_0%,100%_100%,0%_100%)]
             "
-            style={{
-              clipPath:
-                "polygon(0 0, 90% 0, 100% 100%, 0 100%)",
-            }}
           >
       </div>
         </div>
--- Mspr-Canopees/src/pages/About.jsx
+++ Mspr-Canopees/src/pages/About.jsx
@@ -1,5 +1,116 @@
+import notreHistoire from "../assets/About/notre-histoire.png"
+import nosValeurs from "../assets/About/nos-valeurs.png"
+import notreVision from "../assets/About/notre-vision.png"
+import Carousel from "../components/Carousel";
+import carousel1 from "../assets/About/carouselAbout1.png"
+import carousel2 from "../assets/About/carouselAbout2.png"
+import carousel3 from "../assets/About/carouselAbout3.png"
+import carousel4 from "../assets/About/carouselAbout4.png"
+import carousel5 from "../assets/About/carouselAbout5.png"
+import carousel6 from "../assets/About/carouselAbout6.png"
+import carousel7 from "../assets/About/carouselAbout7.png"
+
 export default function About() {
+      const photos = [
+      carousel1,
+      carousel2,
+      carousel3,
+      carousel4,
+      carousel5,
+      carousel6,
+      carousel7,
+    ];
+
   return (
-    <div>About</div>
+    <>
+            <section 
+            className="
+              flex flex-col text-center
+              max-w-150 mx-auto px-5 pt-12 pb-38 box-content
+              lg:pt-18 lg:pb-30
+              lg:flex-row lg:text-left lg:max-w-300 lg:py-26
+            ">
+              <img
+                src={notreHistoire}
+                className="
+                object-contain
+                max-w-88 w-full 
+                mx-auto mt-11
+                lg:mt-0 lg:mx-0 lg:mr-26 lg:max-w-135 lg:w-135 lg:max-h-130 lg:h-130
+              "
+                alt="part of the canopees team"
+              />
+              <div 
+                className="
+                mb-0
+                lg:mt-0 lg:mx-0 lg:mr-26 lg:max-w-135 lg:w-135 lg:max-h-130 lg:h-130
+              ">
+                <h2>Notre histoire</h2>
+                <p>Depuis 2020, Canopées entretiennent les jardins.</p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacus viverra, pretium metus et, condimentum leo. Sed lacus sapien, accumsan ut tellus vel, sollicitudin aliquam ligula. Donec ex metus, vehicula in magna eu, semper pulvinar felis. Curabitur elementum gravida leo, ut fringilla est placerat eu. Suspendisse potenti. Maecenas sapien justo, semper non finibus sit amet, elementum nec arcu. Phasellus vitae mi mollis, rhoncus ex aliquam, pulvinar sem.
+                </p>
+              </div>
+            </section>
+            <section 
+            className="
+              flex flex-col text-center
+              max-w-150 mx-auto px-5 pt-12 pb-38 box-content
+              lg:pt-18 lg:pb-30
+              lg:flex-row lg:text-left lg:max-w-300 lg:py-26
+            ">
+              <div 
+                className="
+                mb-0
+                lg:mt-0 lg:mx-0 lg:mr-26 lg:max-w-135 lg:w-135 lg:max-h-130 lg:h-130
+              ">
+                <h2>Nos valeurs</h2>
+                <p>
+                  Praesent lorem lorem, placerat non orci a, faucibus posuere turpis. Nunc nec pharetra risus, vitae aliquam eros. Fusce venenatis nibh finibus, molestie tellus sed, gravida nisl. Quisque sit amet purus sagittis, mattis augue vitae, sodales erat. Fusce vel pellentesque erat. In ultrices, nunc ut tristique ultricies, libero nibh finibus nisi, ac faucibus velit velit et leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam dapibus pretium risus, id condimentum libero gravida efficitur. Aenean vestibulum interdum dui, vel molestie nunc blandit non. Maecenas vel facilisis arcu. Sed varius tristique ultricies. Nulla facilisi. Proin sit amet nunc non urna sodales finibus ut id leo. Fusce in mattis neque, nec gravida justo.
+                </p>
+              </div>
+              <img
+                src={nosValeurs}
+                className="
+                object-contain
+                max-w-88 w-full 
+                mx-auto mt-11
+                lg:mt-0 lg:mx-0 lg:mr-26 lg:max-w-135 lg:w-135 lg:max-h-130 lg:h-130
+              "
+                alt="part of the canopees team"
+              />
+            </section>
+            <section className="flex-col max-w-300 px-5 mx-auto box-content flex">
+              <Carousel photos={photos} startIndex={3}/>
+            </section>
+            <section 
+            className="
+              flex flex-col text-center
+              max-w-150 mx-auto px-5 pt-12 pb-38 box-content
+              lg:pt-18 lg:pb-30
+              lg:flex-row lg:text-left lg:max-w-300 lg:py-26
+            ">
+              <img
+                src={notreVision}
+                className="
+                object-contain
+                max-w-88 w-full 
+                mx-auto mt-11
+                lg:mt-0 lg:mx-0 lg:mr-26 lg:max-w-135 lg:w-135 lg:max-h-130 lg:h-130
+              "
+                alt="part of the canopees team"
+              />
+              <div 
+                className="
+                mb-0
+                lg:mt-0 lg:mx-0 lg:mr-26 lg:max-w-135 lg:w-135 lg:max-h-130 lg:h-130
+              ">
+                <h2>Notre Vision</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id lacus viverra, pretium metus et, condimentum leo. Sed lacus sapien, accumsan ut tellus vel, sollicitudin aliquam ligula. Donec ex metus, vehicula in magna eu, semper pulvinar felis. Curabitur elementum gravida leo, ut fringilla est placerat eu. Suspendisse potenti. Maecenas sapien justo, semper non finibus sit amet, elementum nec arcu. Phasellus vitae mi mollis, rhoncus ex aliquam, pulvinar sem.
+                </p>
+              </div>
+            </section>
+    </>
   )
 }
